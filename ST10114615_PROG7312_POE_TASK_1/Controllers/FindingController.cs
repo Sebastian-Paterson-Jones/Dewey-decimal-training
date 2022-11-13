0a99d79f441eb7182ae4caeaa77555c666a0305b
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2022-11-13 17:32:31 +0200

    Initital commit

---
+++ ST10114615_PROG7312_POE_TASK_1/Controllers/FindingController.cs
@@ -0,0 +1,21 @@
+﻿using ST10114615_PROG7312_POE_TASK_1.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ST10114615_PROG7312_POE_TASK_1.Controllers
+{
+    public class FindingController : Controller
+    {
+        private DeweyTree tree = new DeweyTree("parent", "root node");
+
+
+        // GET: Finding
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ST10114615_PROG7312_POE_TASK_1/Utils/Tree.cs
@@ -0,0 +1,28 @@
+﻿using ST10114615_PROG7312_POE_TASK_1.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ST10114615_PROG7312_POE_TASK_1.Utils
+{
+    class DeweyTree
+    {
+        public string number;
+        public string description;
+        public List<DeweyTree> children;
+
+        public DeweyTree(string number, string description)
+        {
+            this.number = number;
+            this.description = description;
+            this.children = new List();
+        }
+
+        public void AddChild(string number, string description)
+        {
+            children.add(new DeweyTree(number, description));
+        }
+    }
+}
